63171e45a7f6fdeda8e481fb529d45e1336a3b1a
Steven Hicks <steven.hicks@camunda.com>
2023-08-01 17:22:48 -0500

    basic implementation of all three service task workers

--- index.js
+++ index.js
@@ -8,8 +8,51 @@
 
 // this will read configuration from a file named .env -- that file should contain all your environment variables, like "export ZEEBE_ADDRESS='....'"
 require('dotenv').config();
+const ZB = require('zeebe-node');
 
-// TODO Christina: write the exercise code here!!!
+const zbc = new ZB.ZBClient();
+
+const customerCredit = 87;
+
+function registerHandler(taskName, handler) {
+  zbc.createWorker({
+    taskType: taskName,
+    taskHandler: handler,
+  });
+}
+
+registerHandler('retrieve-customer-balance', retrieveCustomerCreditHandler);
+function retrieveCustomerCreditHandler(job) {
+  console.log(`retrieving customer credit: ${customerCredit}`);
+
+  const result = {
+    customerCredit,
+  };
+
+  return job.complete(result);
+}
+
+registerHandler('apply-credit', applyCreditHandler);
+function applyCreditHandler(job) {
+  const { customerCredit, orderTotal } = job.variables;
+
+  const remainingBalance = orderTotal - customerCredit;
+
+  console.log(`applying credit: ${remainingBalance} remaining.`);
+
+  const result = {
+    remainingBalance,
+  };
+
+  return job.complete(result);
+}
+
+registerHandler('charge-credit-card', chargeCreditCardHandler);
+function chargeCreditCardHandler(job) {
+  console.log(`cha ching $$$$$ ${job.variables.remainingBalance}`);
+
+  return job.complete();
+}
 
 // and ignore all this stuff....
 // getTopology();
