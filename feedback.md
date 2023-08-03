53925ac1375d0a1c1448e4dee9abadeaef0c29ee
Steven Hicks <steven.hicks@camunda.com>
2023-08-03 17:16:31 -0500

    handle errors

---
+++ feedback.md
@@ -0,0 +1,4 @@
+- it's easy to fall behind, and once you're behind, there's no obvious way to get back on track.
+  - are there any existing assets that you share with customers to get them back on track?
+  - or links to working examples at least to look at?
+  - do you do breakout rooms? It'd be nice to work as a small group, in case anyone has fallen off track, they can still see things happening with someone else driving.
--- ingo/chargeCreditCard.js
+++ ingo/chargeCreditCard.js
@@ -8,6 +8,14 @@ export function register(zbc) {
 function chargeCreditCardHandler(job) {
   const { cardNumber, cvc, expiryDate, remainingBalance } = job.variables;
 
+  if (expiryDate.length != 5) {
+    console.error('ERROR: Invalid credit card expiry date!');
+    return job.error({
+      errorCode: 'creditCardChargeError',
+      errorMessage: 'Invalid credit card expiry date.',
+    });
+  }
+
   console.log(`cha ching $$$$$`);
   console.log(`  credit card number: ${cardNumber}`);
   console.log(`  cvc: ${cvc}`);
